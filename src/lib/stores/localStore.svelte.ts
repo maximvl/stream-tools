8f51d08659d27b909525fd769fdab46a320ef0a1
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-04-19 13:27:54 +0200

    Implement cross-tab synchronization for LocalStore
    The `LocalStore` now synchronizes its state across browser tabs or
    windows
    using `localStorage` and custom `window` events. This ensures that
    changes
    made in one tab are reflected in others without requiring a page reload.
    
    A new `/test` page has been added to demonstrate the `LocalStore`'s
    capabilities with various input types (text, number, range, select,
    checkbox, textarea) and provide an interactive environment for testing.

--- src/lib/stores/localStore.svelte.ts
+++ src/lib/stores/localStore.svelte.ts
@@ -1,13 +1,46 @@
+import { untrack } from 'svelte'
+
+let StoreIdCounter = 0
+
+function getStoreId() {
+  StoreIdCounter += 1
+  return StoreIdCounter
+}
+
+type CustomEventData<T> = {
+  value: T
+  emitterId: number
+}
+
 class _LocalStore<T> {
   key: string
   value = $state<T>() as T
+  _storeId: number
 
   constructor(key: string, defaultValue: T | null = null) {
-    this.value = this.loadValue(key, defaultValue)
     this.key = key
+    this.value = this.loadValue(key, defaultValue)
+    this._storeId = getStoreId()
+
+    window.addEventListener(`localStore:${key}`, (event: Event) => {
+      const customEvent = event as CustomEvent<CustomEventData<T>>
+      if (customEvent.detail.emitterId !== this._storeId) {
+        untrack(() => (this.value = customEvent.detail.value))
+      }
+    })
 
     $effect(() => {
-      localStorage.setItem(this.key, JSON.stringify(this.value))
+      const value = this.value
+
+      localStorage.setItem(this.key, JSON.stringify(value))
+      window.dispatchEvent(
+        new CustomEvent<CustomEventData<T>>(`localStore:${this.key}`, {
+          detail: {
+            emitterId: this._storeId,
+            value,
+          },
+        }),
+      )
     })
   }
 
