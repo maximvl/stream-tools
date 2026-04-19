6d7c8ea731a32380251518bd9692d469583101dd
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-04-19 12:34:20 +0200

    Reformat svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,12 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html'
+      fallback: 'index.html',
     }),
     paths: {
-      base: ''
-    }
-  }
+      base: '',
+    },
+  },
 }
 
 export default config
