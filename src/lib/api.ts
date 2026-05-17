a269b57517e9aba4fb1cdca763d6f232f7fa6d06
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-05-17 22:38:20 +0200

    improve mocking

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,12 +2,13 @@ import { makeMessage, MocksManager } from './apiMocks'
 import type { ChatMessage, ChatServer, VkRole, VkRoleId } from './types'
 import random from 'lodash/random'
 import sample from 'lodash/sample'
+import { dev } from '$app/environment'
 
 const URL_PREFIX = '/v2'
 // const URL_PREFIX = 'http://localhost:8088/v2'
 
 // const MOCK_API = import.meta.env.MODE === 'development' && !URL_PREFIX.includes('127.0.0.1')
-const MOCK_API = true
+const MOCK_API = dev
 
 console.log('MOCK_API', MOCK_API)
 
